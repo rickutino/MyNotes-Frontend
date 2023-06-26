1d5b57f6f52fbb185d8f12b8f8cfa67694e3df80
rickutino <rickutino@gmail.com>
2023-06-26 18:01:29 +0900

    feature: add  fecthdata note details feature

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,36 +1,90 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "../../components/Button/input";
 import { ButtonText } from "../../components/ButtonText";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
 import { Container, Content, Links } from "./styles";
+import { api } from "../../services/api";
+
+interface ILinks {
+  id: string;
+  url: string;
+}
+
+interface ITags {
+  id: string;
+  name: string;
+  note_id: string;
+  user_id: string;
+}
+
+interface IDataDTO {
+  title: string;
+  description: string;
+  tags: ITags[];
+  links: ILinks[];
+}
 
 export function Details() {
+  const [data, setData] = useState<IDataDTO>();
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate("/");
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  },[])
   return (
     <Container>
       <Header />
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText title="Delete note" />
+              <h1>{data.title}</h1>
+              <p>{data.description}</p>
+            {
+              data.links &&
+              <Section title="Useful links">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target="_blank">
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </Section>
+            }
 
-      <main>
-        <Content>
-          <ButtonText title="Delete note" />
-            <h1>Introduction to React</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia quidem a officia omnis! Quod, nobis praesentium quibusdam quam blanditiis quas!</p>
-          <Section title="Useful links">
-            <Links>
-              <li><a href="https://rocketseat.com.br">https://rocketseat.com.br</a></li>
-              <li><a href="https://rocketseat.com.br">https://rocketseat.com.br</a></li>
-            </Links>
-          </Section>
-
-          <Section title="Useful links">
-            <Tag title="Nodejs"/>
-            <Tag title="Express"/>
-          </Section>
-          <Button title="MyButton" loading/>
-          <Button title="Back" />
-          <Button title="Home" />
-        </Content>
-      </main>
+            {
+              data.tags &&
+              <Section title="Useful links">
+                {
+                  data.tags.map(tag => (
+                    <Tag key={String(tag.id)} title={tag.name}/>
+                  ))
+                }
+              </Section>
+            }
+            <Button title="Back" onClick={handleBack} />
+          </Content>
+        </main>
+      }
     </Container>
   )
 }
