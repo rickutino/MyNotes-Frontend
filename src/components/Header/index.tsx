4022cd91dcc98c49470f416eae084011b381a208
rickutino <rickutino@gmail.com>
2023-06-27 17:16:47 +0900

    refactor: logout feature, re-direct user after logout to login page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,22 +3,29 @@ import { Container, Profile, Logout } from "./styles";
 import { useAuth } from "../../hook/auth";
 import { api } from "../../services/api";
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
   const { user, signOut } = useAuth();
   const avatarUrl = user?.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+  const navigate = useNavigate();
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
 
   return (
     <Container>
       <Profile to="/profile">
-        <img src={avatarUrl} alt={user.name} />
+        <img src={avatarUrl} alt={user?.name} />
         <div>
           <span>Welcome</span>
-          <strong>{user.name}</strong>
+          <strong>{user?.name}</strong>
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
